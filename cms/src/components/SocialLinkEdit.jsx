3c9c884d782e9f03b10d742c5cd6588e7e15a83f
Jon <thisismbaboool@gmail.com>
2025-11-01 15:26:21 +0700

    fix(cms): Import smartValidation in SocialLinkEdit
    This commit fixes a 'ReferenceError: smartValidation is not defined'
    by importing the necessary validation object from '../utils/notifications.js'
    into 'cms/src/components/SocialLinkEdit.jsx'.

--- cms/src/components/SocialLinkEdit.jsx
+++ cms/src/components/SocialLinkEdit.jsx
@@ -1,6 +1,7 @@
 import { Edit, SimpleForm, TextInput, required } from 'react-admin';
 import { Box, Typography, Paper } from '@mui/material';
 import { SmartFormToolbar } from './SmartFormToolbar';
+import { smartValidation } from '../utils/notifications';
 
 // Validasi untuk form
 const platformValidation = [required(), smartValidation.minLength(2)];
